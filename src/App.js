546fd55b0f05bd67d4703f75ee6fe872ba7c72bc
Mir Saidi <mirnumbing@gmail.com>
2022-04-07 19:53:37 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import About from './pageComponent/About'
+import Home from './pageComponent/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/about" component={About} />
+
+
+
+          </Switch>
+
+        </div>
+      </Router >
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/pageComponent/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function About() {
+  return (
+    <h1>hey we are ironhack this is about us</h1>
+  )
+}
+
+export default About
