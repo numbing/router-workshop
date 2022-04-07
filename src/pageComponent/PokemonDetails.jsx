ab62b17d670bb48ae348fcfced40195b626647f6
Mir Saidi <mirnumbing@gmail.com>
2022-04-07 21:17:52 +0200

    pokemon details display

--- src/pageComponent/PokemonDetails.jsx
+++ src/pageComponent/PokemonDetails.jsx
@@ -1,9 +1,41 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
 export default class PokemonDetails extends Component {
+
+  state = {
+    pokemon: {},
+    loading: true
+  }
+
+
+
+  componentDidMount() {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${this.props.match.params.pokeName}`).then(responseFromPokemon => {
+      console.log("responseFromPokemon", responseFromPokemon);
+
+      this.setState({
+        pokemon: responseFromPokemon.data,
+        loading: false
+
+      })
+    })
+  }
+
   render() {
+    console.log(this.state.pokemon);
     return (
-      <div>PokemonDetails</div>
+      <div>
+        {this.state.loading ? <h1>Loading.....</h1> :
+          <div>
+            <h1>{this.state.pokemon.name}</h1>
+            <p>{this.state.pokemon.height}</p>
+            <img src={this.state.pokemon.sprites.front_default} alt="" />
+          </div>
+
+        }
+      </div>
+
+
     )
   }
 }
